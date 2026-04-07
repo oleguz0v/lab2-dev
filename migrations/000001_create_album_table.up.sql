cf0be9f76f1e65ee979f0a500a25d1c4fd3958ee
oleguz0v <podguzov.on@phystech.edu>
2026-04-07 13:36:37 +0300

    Create 000001_create_album_table.up.sql

---
+++ migrations/000001_create_album_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS album (
+  id         SERIAL PRIMARY KEY,
+  title      VARCHAR(255) NOT NULL,
+  artist     VARCHAR(255) NOT NULL,
+  price      REAL NOT NULL
+);
+
+INSERT INTO album
+  (title, artist, price)
+VALUES
+  ('Blue Train', 'John Coltrane', 56.99),
+  ('Giant Steps', 'John Coltrane', 63.99),
+  ('Jeru', 'Gerry Mulligan', 17.99),
+  ('Sarah Vaughan', 'Sarah Vaughan', 34.98);
